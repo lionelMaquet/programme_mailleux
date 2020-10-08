85edca1d59bd20166c815c2f3e29710843b48863
Lionel Maquet <lionel.maquet@gmail.com>
2020-10-08 15:11:11 +0200

    end of project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace programme_mailleux
 {
@@ -6,7 +10,52 @@ namespace programme_mailleux
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string mainPath = args[0];
+            string[] subdirectories = Directory.GetDirectories(mainPath);
+
+            foreach(string directory in subdirectories)
+            {
+                var dir = new DirectoryInfo(directory);
+                string[] files = Directory.GetFiles(directory);
+
+                var dirName = dir.Name;
+                string mainPhoto = "";
+                string picto = "";
+                List<String> otherPhotos = new List<String>{};
+                string otherPhotosConc = "";
+
+                foreach(string fileName in files){
+                    // main photo 
+                    if (fileName.EndsWith("photo-00.jpg"))
+                    {
+                        mainPhoto = $"{Path.GetFileNameWithoutExtension(fileName)}.jpeg";
+                    }
+
+                    // picto 
+                    if (fileName.EndsWith("picto.jpg"))
+                    {
+                        picto = $"{Path.GetFileNameWithoutExtension(fileName)}.jpeg";
+                    }
+
+                    // autres 
+                    else if (fileName.EndsWith(".jpg"))
+                    {
+                        otherPhotos.Add($"{Path.GetFileNameWithoutExtension(fileName)}.jpeg");
+                    } 
+                }
+                otherPhotosConc = String.Join(";", otherPhotos);
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter($@"{mainPath}/liens_photos.txt", true))
+                {
+                    file.WriteLine(dirName);
+                    file.WriteLine(mainPhoto);
+                    file.WriteLine(picto);
+                    file.WriteLine(otherPhotosConc);
+                    file.WriteLine("");
+                    file.WriteLine("");
+                }
+            }
         }
     }
 }
